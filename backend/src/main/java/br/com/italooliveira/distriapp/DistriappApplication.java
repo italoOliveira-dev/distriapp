91e5e155de10716832f6e430d12e7c00231598b7
Italo Oliveira <italooliveira-dev@hotmail.com>
2025-11-01 10:43:52 -0300

    feat: initial backend

---
+++ backend/src/main/java/br/com/italooliveira/distriapp/DistriappApplication.java
@@ -0,0 +1,13 @@
+package br.com.italooliveira.distriapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistriappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistriappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/italooliveira/distriapp/DistriappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.italooliveira.distriapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistriappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
